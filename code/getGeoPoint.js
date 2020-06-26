a5f572518a0d1beea72ae2335d5df1a24a60c685
Laura Aguilera <lac743@inlumine.ual.es>
2020-06-26 04:00:34 +0200

    Proyecto terminado

--- code/getGeoPoint.js
+++ code/getGeoPoint.js
@@ -1,6 +1,14 @@
-module.exports.function = function getGeoPoint (geoPoint, mensajeAlerta) {
-  return {
-    geoPoint: geoPoint,
-    mensajeAlerta: mensajeAlerta
+var console = require('console')
+
+module.exports.function = function getGeoPoint(geoPoint, mensajeAlerta, userData) {
+  if (userData) {
+    if (mensajeAlerta.equals(userData.mensajeAlertaConfig)) {
+      return {
+        contacto: userData.contacto,
+        ubicacion: "https://www.latlong.net/c/?lat" + geoPoint.latitude + "&long=" + geoPoint.longitude,
+        geoPoint: geoPoint
+      }
+    }
   }
+  return null
 }
